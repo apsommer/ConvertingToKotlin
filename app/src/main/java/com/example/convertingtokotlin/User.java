2858e0f5b3af6fce0731153dd6e308429c3d37eb
Drew Sommer <andrew.sommer@tiaa.org>
2021-07-15 12:16:49 -0400

    feat: Model user

---
+++ app/src/main/java/com/example/convertingtokotlin/User.java
@@ -0,0 +1,30 @@
+package com.example.convertingtokotlin;
+
+import androidx.annotation.Nullable;
+
+public class User {
+
+    @Nullable private String firstName;
+    @Nullable private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
